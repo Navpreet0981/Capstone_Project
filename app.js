cc1349693e4afc365408cf7cbd69ad3584b82247
Navpreet0981 <navi2005saini@gmail.com>
2024-10-21 20:15:56 +0530

    adding data from form and posting back to home page using post request and arrays

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
+
+
+let items= [];
+app.post("/",(req, res) =>{
+    const name = req.body.itemName;
+    const time = req.body.itemTime;
+    const date = req.body.itemDate;
+    const description = req.body.itemDescription;
+
+    items.push({name, time, date, description});
+    res.redirect("/");
+});
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    console.log(items)
+    res.render("index.ejs", { data: items});
 });
 
 app.listen(port, () => {
