d8ab3c3617ee2357bb08c4b794e12c6ae5c33a3b
Navpreet0981 <navi2005saini@gmail.com>
2024-12-25 21:17:35 +0530

    Hiding Passwords Etc for Security

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const port = 4000;
 env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
